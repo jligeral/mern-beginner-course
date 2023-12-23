56d04826c1a4346b374448d5928e862c563d5d93
James Ligeralde <jligeral@hawaii.edu>
2023-12-22 21:14:21 -1000

    Loading and empty states added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import { Note as NoteSchema } from './models/note';
 import Note from "./components/Note";
-import {Button, Col, Container, Row} from 'react-bootstrap';
+import {Button, Col, Container, Row, Spinner} from 'react-bootstrap';
 import styles from './styles/NotesPage.module.css';
 import styleUtils from './styles/utils.module.css';
 import * as NotesApi from './network/notes_api';
@@ -14,16 +14,22 @@ function App() {
 
   const [showCreateNote, setShowCreateNote] = useState(false);
   const [noteToEdit, setNoteToEdit] = useState<NoteSchema | null>(null);
+  const [notesLoading, setNotesLoading] = useState(false);
+  const [showNotesLoadingError, setShowNotesLoadingError] = useState(false);
 
   useEffect(() => {
     async function getNotes() {
       try {
+        setShowNotesLoadingError(false); // <-- hide the error message
+        setNotesLoading(true); // <-- show the loading spinner
         const notes = await NotesApi.fetchNotes();
         setNotes(notes);
 
       } catch (error) {
         console.error(error);
-        alert(error);
+        setShowNotesLoadingError(true);
+      } finally { // <-- this block of code will always run
+        setNotesLoading(false); // <-- hide the loading spinner
       }
     }
     getNotes();
@@ -39,15 +45,8 @@ function App() {
     }
   }
 
-  return (
-    <Container>
-      <Button
-        className={`mb-3 ${styleUtils.blockCenter} ${styleUtils.flexCenter}`}
-        onClick={() => setShowCreateNote(true)}>
-        <FaPlus />
-        Create New Note
-      </Button>
-      <Row xs={1} md={2} xl={3} className="g-4">
+  const notesGrid =
+      <Row xs={1} md={2} xl={3} className={`g-4 ${styles.notesGrid}`}>
         {notes.map((note) => (
           <Col key={note._id}> {/* The key is a unique identifier to each note */}
             <Note
@@ -59,6 +58,25 @@ function App() {
           </Col>
         ))}
       </Row>
+
+  return (
+    <Container className={styles.notesPage}>
+      <Button
+        className={`mb-3 ${styleUtils.blockCenter} ${styleUtils.flexCenter}`}
+        onClick={() => setShowCreateNote(true)}>
+        <FaPlus />
+        Create New Note
+      </Button>
+      {notesLoading && <Spinner animation="border" variant="primary" />}
+      {showNotesLoadingError && <p>Error loading notes. Try refreshing.</p>}
+      {!notesLoading && !showNotesLoadingError &&
+      <> {/* React fragment used to put curly braces inside curly braces */}
+        {notes.length > 0
+          ? notesGrid
+          : <p>There are no notes to show yet</p>
+        }
+      </>
+      }
       {
         showCreateNote &&
         <CreateEditNote
