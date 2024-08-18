a0ab242a2d82588fa74fae0adfa4fac0456c2d58
James Ligeralde <jligeral@hawaii.edu>
2024-08-17 18:30:41 -1000

    Update App.tsx
    Login, Sign Up, and Logout functions working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,33 +4,66 @@ import styles from './styles/NotesPage.module.css';
 import SignUpModal from './components/SignUpModal';
 import LoginModal from "./components/LoginModal";
 import NavBar from "./components/NavBar";
+import { User } from './models/user';
+import * as NotesApi from './network/notes_api';
+import NotesPageLoggedInView from "./components/NotesPageLoggedInView";
+import NotesPageLoggedOutView from "./components/NotesPageLoggedOutView";
 
 function App() {
+
+  const [loggedInUser, setLoggedInUser] = useState<User|null>(null);
+
+  const [showSignUpModal, setShowSignUpModal] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(false);
+
+  useEffect(() => {
+    async function fetchLoggedInUser() {
+      try {
+          const user = await NotesApi.getLoggedInUser();
+          setLoggedInUser(user);
+      } catch (error) {
+          console.error(error);
+      }
+    }
+    fetchLoggedInUser();
+  }, []);
 
   return (
     <div>
       <NavBar
-        loggedInUser={null}
-        onSignUpClicked={() => {}}
-        onLoginClicked={() => {}}
-        onLogoutSuccessful={() => {}}
+        loggedInUser={loggedInUser}
+        onSignUpClicked={() => setShowSignUpModal(true)}
+        onLoginClicked={() => setShowLoginModal(true)}
+        onLogoutSuccessful={() => setLoggedInUser(null)}
       />
       <Container className={styles.notesPage}>
-        {
-          false &&
-          <SignUpModal
-          onDismiss={() => {}}
-          onSignUpSuccess={() => {}}
-          />
-        }
-        {
-          false &&
-          <LoginModal
-            onDismiss={() => {}}
-            onLoginSuccess={() => {}}
-          />
-        }
+        <>
+          {loggedInUser // Check if user is logged in and display appropriate page view
+            ? <NotesPageLoggedInView />
+            : <NotesPageLoggedOutView />
+          }
+        </>
       </Container>
+      {
+        showSignUpModal &&
+        <SignUpModal
+          onDismiss={() => setShowSignUpModal(false)}
+          onSignUpSuccess={(user) => {
+            setLoggedInUser(user);
+            setShowSignUpModal(false)
+          }}
+        />
+      }
+      {
+        showLoginModal &&
+        <LoginModal
+          onDismiss={() => setShowLoginModal(false)}
+          onLoginSuccess={(user) => {
+            setLoggedInUser(user);
+            setShowLoginModal(false)
+          }}
+        />
+      }
     </div>
   );
 }
